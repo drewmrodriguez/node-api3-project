cd8f1c18fbb180b60359f8e4213a14ab1b097c1a
drewmrodriguez <drewmrodriguez@gmail.com>
2021-07-13 21:32:04 -0500

    server set up

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -1,4 +1,10 @@
 const express = require('express');
+const express = require("express");
+const {
+    validateUserId,
+    validateUser,
+    validatePost,
+} = require("../middleware/middleware");
 
 // You will need `users-model.js` and `posts-model.js` both
 // The middleware functions also need to be required
